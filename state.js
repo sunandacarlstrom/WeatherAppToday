163505683620f51e446c1df09cf190c7c123c21b
Sunanda Carlström <112494765+sunandacarlstrom@users.noreply.github.com>
2023-11-05 19:21:32 +0100

    Update state.js
    Lade till "s" så att det står "https" för båda hämtningar från API:et. Detta för att det inte gick att göra ett "HTTP"-request från en "HTTPS"-sida. Felmeddelande: Mixed Content.

--- state.js
+++ state.js
@@ -4,7 +4,7 @@ const state = {
     api: {
         key: "0c25f21f6f5eb3a3460e633abf9691de",
         weatherUrl: "https://api.openweathermap.org/data/2.5/",
-        geoCodingUrl: "http://api.openweathermap.org/geo/1.0/direct?q=",
+        geoCodingUrl: "https://api.openweathermap.org/geo/1.0/direct?q=",
     },
 };
 
